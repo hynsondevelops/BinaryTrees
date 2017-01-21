831d086f79e3604012e62b8f91d171661790890e
Adam Hynson <hynsondevelops@gmail.com>
2017-01-21 10:16:00 -0500

    Recursive DFS Implemented

--- Tree.rb
+++ Tree.rb
@@ -7,11 +7,9 @@ class Tree
 	def build_tree(array)
 		@root = Node.new(array[0])
 		for i in 1...array.size
-			puts "placing #{array[i]}"
 			current = @root
 			placing = true
 			while (placing)
-				puts current.value
 				if (current.value <= array[i])
 					if (current.right == nil)
 						current.right = Node.new(array[i])
@@ -27,7 +25,6 @@ class Tree
 				end
 			end
 		end
-		puts "Done"
 	end
 
 	def initialize(array = nil)
@@ -42,13 +39,7 @@ class Tree
 		set = Array.new
 		set.push(@root)
 		while (queue.size != 0)
-			puts "\nQueue: "
-			for i in 0...queue.size
-				print queue[i].value
-				print ", "
-			end
 			currentNode = queue[0]
-			#puts currentNode.value
 			if (currentNode.value == value)
 				return currentNode
 			end
@@ -70,27 +61,37 @@ class Tree
 		stack.push(@root)
 		searching = true
 		while (stack.size != 0)
-			for i in 0...stack.size
-				print stack[i].value
-				print ", "
-			end
 			currentNode = stack.pop
 			if (currentNode.value == value)
 				return currentNode
 			end
-			if (currentNode.left != nil && stack)
+			if (currentNode.left != nil)
 				stack.push(currentNode.left)
 			end
 			if (currentNode.right != nil)
 				stack.push(currentNode.right)
 			end
 		end
+		return nil
+	end
+
+	def dfs_rec(currentNode, value)
+	    if (currentNode.value == value)
+	        return currentNode
+	    end
+	    if (currentNode.left != nil)
+	    	leftNode = dfs_rec(currentNode.left, value)
+	    end
+	    if (currentNode.right != nil)
+	    	rightNode = dfs_rec(currentNode.right, value)
+	    end
+	    return (leftNode or rightNode)
 	end
 
 end
 
-t = Tree.new([3, 1, 5, 7, 12, 1, 6])
-node = t.breadth_first_search(55)
+t = Tree.new([3, 1, 5, 7, 12, 8, 6])
+node = t.depth_first_search(44)
 if (node == nil)
 	puts "SUCCESS"
 end
@@ -98,3 +99,5 @@ end
 puts "\n\n\n"
 newNode = t.depth_first_search(12)
 puts "\n#{newNode.value}"
+
+puts t.dfs_rec(t.root, 12)
